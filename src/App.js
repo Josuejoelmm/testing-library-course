4244d13c2129bae8eb34c543b8c70e8d93f4244c
Josue Mendez <josue.mendez@mercadolibre.com>
2022-04-17 21:54:44 -0300

    Test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [isDisabledButton, setisDisabledButton] = useState(false);
   const newColorButton = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
     <div>
       <button
         style={{ backgroundColor: buttonColor }}
-        onClick={() => setButtonColor(newColorButton)}>
+        onClick={() => setButtonColor(newColorButton)}
+        {...(isDisabledButton ? { disabled: isDisabledButton } : {})}
+      >
           {`Change to ${newColorButton}`}
-      </button>  
+      </button>
+      <input
+        type="checkbox"
+        id="enable-checkbox"
+        defaultChecked={isDisabledButton}
+        aria-checked={isDisabledButton}
+        onClick={() => setisDisabledButton(!isDisabledButton)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -21,3 +21,33 @@ test('button turns blue when clicked', () => {
 
   expect(colorButton.textContent).toBe('Change to red');
 });
+
+test('Initial conditions for button and checkbox', () => {
+  render(<App />);
+  
+  // button has to be Enabled
+  const buttonEnabled = screen.getByRole('button', { name: 'Change to blue' });
+  
+  expect(buttonEnabled).toBeEnabled();
+
+  // checkbox has to be Unchecked
+  const checkbox = screen.getByRole('checkbox');
+
+  expect(checkbox).not.toBeChecked();
+});
+
+test('Button has to be disabled after checked checkbox', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox');
+  const buttonColor = screen.getByRole('button', { name: 'Change to blue' });
+
+  // checkbox has to be checked, then the button to be disabled
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(buttonColor).toBeDisabled();
+  
+  // Check if togle to enabled the button
+  fireEvent.click(checkbox);
+  expect(buttonColor).toBeEnabled();
+});
